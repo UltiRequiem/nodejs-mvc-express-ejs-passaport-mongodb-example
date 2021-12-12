46f46c49692c272e6a736ea0e7d1596fd95848cd
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-12 07:49:58 -0500

    :recycle: Update

--- controllers/login.js
+++ controllers/login.js
@@ -1,71 +1,77 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 
-export function registerView(_req, { render }) {
-  render("register", {});
+export function registerView(_req, res) {
+  res.render("register", {});
 }
 
-export function loginView(_req, { render }) {
-  render("login", {});
+export function loginView(_req, res) {
+  res.render("login", {});
 }
 
 export function registerUser(
   { body: { name, email, location, password, confirm } },
-  { redirect, render }
+  res
 ) {
   if (!name || !email || !password || !confirm) {
     console.log("Fill empty fields");
+    res.redirect("/login");
   }
+
   if (password !== confirm) {
     console.log("Password must match");
-    redirect("/login");
-  } else {
-    User.findOne({ email: email }).then((user) => {
-      if (user) {
-        console.log("email exists");
-        render("register", {
-          name,
-          email,
-          password,
-          confirm,
-        });
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          location,
-          password,
-        });
-        bcrypt.genSalt(10, (err, salt) => {
-          if (err) throw err;
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) {
-              throw err;
-            }
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(redirect("/login"))
-              .catch((err) => console.log(err));
-          });
-        });
-      }
-    });
+    res.redirect("/login");
+    return;
   }
+
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      console.log("email exists");
+      res.render("register", {
+        name,
+        email,
+        password,
+        confirm,
+      });
+      return;
+    }
+
+    const newUser = new User({
+      name,
+      email,
+      location,
+      password,
+    });
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) throw err;
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(res.redirect("/login"))
+          .catch((err) => console.log(err));
+      });
+    });
+  });
 }
 
-export function loginUser({ body: { email, password } }, res) {
+export function loginUser(req, res) {
+  const { email, password } = req.body;
+
   if (!email || !password) {
     console.log("Please fill in all the fields");
     res.render("login", {
       email,
       password,
     });
-  } else {
-    passport.authenticate("local", {
-      successRedirect: "/dashboard",
-      failureRedirect: "/login",
-      failureFlash: true,
-    })(req, res);
+
+    return;
   }
+
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })(req, res);
 }
